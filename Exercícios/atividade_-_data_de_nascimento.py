41ca631ebb19bba3f778d3c08a4bc08b697fab9c
f1rst <dossantos.hiago10@gmail.com>
2024-01-25 22:36:07 -0300

    exercicio - data de nascimento

---
+++ Exercícios/atividade_-_data_de_nascimento.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        nome = input('Qual o seu nome completo? ')
+        ano_nascimento = int(input('Em que ano você nasceu? '))
+        ano_atual = 2024
+        if ano_nascimento >= 1922 and ano_nascimento <= 2023:
+            idade = ano_atual - ano_nascimento
+            print(f'Seu nome completo é: {nome}.')
+            if idade == 1:
+                print(f'Você irá completar {idade} ano em 2024.')
+            else: 
+                print(f'Você irá completar {idade} anos em 2024.')
+        else:
+            print('Ano de nascimento inválido!')
+    except:
+        print('Caracter incorreto!')
+            
